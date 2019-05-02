fad0779bd9984ccd26c9d9243468f091c95cb247
TsungKaiHo <50210177+TsungKaiHo@users.noreply.github.com>
2019-05-03 00:16:37 +0800

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,28 @@
+//
+//  main.c
+//  進階題 1
+//
+//  Created by 何宗愷 on 2019/4/26.
+//  Copyright © 2019 何宗愷. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    
+        char n;
+        for (n=65; n<=90; n++)
+        {
+            printf("%c ",n);
+        }
+        
+        printf("\n");
+        for (n=97; n<=122; n++)
+        {
+            printf("%c ",n);
+        }
+        printf("\n");
+        system ("pause");
+        return 0;
+}
